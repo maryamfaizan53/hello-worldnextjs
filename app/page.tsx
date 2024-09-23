1c1ca21deae94cb87d06120f95c669468f2a5b84
Maryam faizan <118754207+maryamfaizan53@users.noreply.github.com>
2024-09-24 02:20:40 +0500

    done

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const  Home= () => {
+  return (
+    <>
+    <div className='bg-black hover:bg-black h-screen items-center justify-center flex' >
+      <div className='bg-stone-700 hover:bg-slate-500 h-1/2 max-w-96  flex items-center justify-center border-x-gray-200'>
+    <h1 className='text-6xl font-extrabold text-white hover:text-black'>Hello World</h1>
+    </div>
+    </div>
+    </>   
+  )
+}
+
+export default Home
